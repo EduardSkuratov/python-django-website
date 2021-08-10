56436d864ba9746ddc6ab3b1ed75437dc51302cb
Skuratov Eduard <skuratov.eduard.dim@gmail.com>
2021-08-10 16:30:50 +0300

    3commit

--- board/views.py
+++ board/views.py
@@ -1,9 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Bb
 
 def index(request):
-    s = 'List of announcements\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'board/index.html', {'bbs': bbs})
